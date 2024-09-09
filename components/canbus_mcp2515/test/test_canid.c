db63f0f58de8efdce5a576affc88c87c892e7518
Gilles Zunino <gilleszunino@users.noreply.github.com>
2024-09-08 20:22:00 -0700

    Add component unit tests

---
+++ components/canbus_mcp2515/test/test_canid.c
@@ -0,0 +1,15 @@
+// -----------------------------------------------------------------------------------
+// Copyright 2024, Gilles Zunino
+// -----------------------------------------------------------------------------------
+
+
+#include "unity.h"
+
+
+TEST_CASE("CAN Standard ID Encoding", "[canid]") {
+    // TODO: Add test case
+}
+
+TEST_CASE("CAN Extended ID Encoding", "[canid]") {
+    // TODO: Add test case
+}
